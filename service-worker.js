615c8bd11ae19ac07231f738a20272bb3c7f1c9c
junwei88 <j.wei88@gmail.com>
2025-12-21 14:05:10 +0800

    Delete old caches when activated

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,35 @@
 const CACHE_NAME = 'noms-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
 
+// INSTALL
 self.addEventListener('install', event => {
+event.waitUntil(
+  caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+);
+self.skipWaiting(); // optional but recommended
+});
+
+// ACTIVATE
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './app.js',
-        './manifest.json'
-      ])
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME)
+            .map(k => caches.delete(k))
+      )
     )
   );
 });
 
+// FETCH
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(res => res || fetch(event.request))
@@ -24,4 +40,3 @@ self.addEventListener('fetch', event => {
 // self.addEventListener('fetch', event => {
 //   event.respondWith(fetch(event.request));
 // });
-  
